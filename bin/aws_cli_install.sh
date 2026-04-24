040b505a124e5a5da021cee1f922ca648f6aedc7
Landry KAMHOUA <lkamhoua@gmail.com>
2026-04-24 12:00:16 +0000

    Installation & Configuration de AWS CLI

---
+++ bin/aws_cli_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd /workspaces
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+cd $CODESPACE_VSCODE_FOLDER
